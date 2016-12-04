b68838c5de88ad9b9441f74fe93e3e1aa9e4453f
Tung Thanh Le <ttungl@users.noreply.github.com>
2016-12-03 20:05:55 -0600

    Add files via upload

---
+++ 65-valid-number.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/valid-number/
+// #65
+
+public class Solution {
+    public boolean isNumber(String s) {
+        // check isEmpty
+        if(s==null) return false;
+        // method 1: use regex analysis (picked).
+        // method 2: use substring/(charAt() and isDigit()) to analyze each element in string s. (optional)
+        
+        String regex = "[-+]?(\\d+\\.?|\\.\\d+)\\d*(e[-+]?\\d+)?";
+        
+        if (s.trim().matches(regex)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
