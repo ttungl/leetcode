a2f721111c8eb66baa6a01081870acc2015d80ab
Tung Thanh Le <ttungl@users.noreply.github.com>
2016-12-14 21:20:48 -0600

    Update 65-valid-number.java

--- 65-valid-number.java
+++ 65-valid-number.java
@@ -2,14 +2,12 @@
 // #65
 // Note: trim() is used to return a copy of the string \
 //       without leading and trailing whitespaces.
-
 public class Solution {
     public boolean isNumber(String s) {
         // check isEmpty
         if(s==null) return false;
         // method 1: use regex analysis (picked).
         // method 2: use substring/(charAt() and isDigit()) to analyze each element in string s. (optional)
-        
         String regex = "[-+]?(\\d+\\.?|\\.\\d+)\\d*(e[-+]?\\d+)?";
         
         if (s.trim().matches(regex)){
