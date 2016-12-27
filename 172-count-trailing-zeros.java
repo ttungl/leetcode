e4414bf69a98c11052f613fd1ee8f95820315ca0
Tung Thanh Le <ttungl@users.noreply.github.com>
2016-12-27 17:10:53 -0600

    Update 172-count-trailing-zeros.java

--- 172-count-trailing-zeros.java
+++ 172-count-trailing-zeros.java
@@ -4,8 +4,6 @@
 // Thus, only count 5s.
 // Trailing 0s in n! = Count of 5s in prime factors of n!
 //                   = floor(n/5) + floor(n/25) + floor(n/125) + ....
-
-
 public class Solution {
     public int trailingZeroes(int n) {
         int counter = 0;
